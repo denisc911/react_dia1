8ec5460ad64e3ff0d2bfa7f7c39b1673f1effec5
denisc911 <denis.cabrera1990@gmail.com>
2024-07-23 19:16:30 +0200

    ya funcionando mejor

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import Person from "./components/Person"
+import PersonClass from "./components/PersonClass"
+import Home from "./components/Home"
+import TheHeader from "./components/Header"
+
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-       
-      </div>
-         </>
+<div className="App">
+<TheHeader title="PERSONAL PAGE" />
+<Home title="HOME" name="Denis" surname="Cabrera" age="33" passion="Tecnologia" currentLearning="FullStack web development" matter="React" interestedIn="Developement" hobby="Escuchar Musica" />
+
+
+  <h1 className="header h1">Person</h1>
+  <Person name="Peter" surname="Parker" age="33"/>
+  <Person name="Jonh" surname="travolta" age="26"/>
+  <Person name="Michael" surname="Jackson" age="40"/>
+  <Person name="John" surname="Salchichon" age="40"/>
+  <h1 className="header h1">Person Class</h1>
+  <PersonClass name="Martin" surname="Gonzales" age="18"/>
+  <PersonClass name="Janina" surname="Martinez" age="20"/>
+  <PersonClass name="Joe" surname="Smith" age="53"/>
+
+</div>
   )
 }
 
